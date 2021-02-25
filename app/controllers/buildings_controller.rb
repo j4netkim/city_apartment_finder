1123f05057cb99a04541994b3d5d1dacae1377f7
J K <jkim65@buffalo.edu>
2021-02-24 20:29:59 -0500

    add relationships to models

---
+++ app/controllers/buildings_controller.rb
@@ -0,0 +1,46 @@
+class BuildingsController < ApplicationController
+
+    def new
+        @owner = Owner.find(params[:owner_id])
+        @building = Building.new
+        @url = new_owner_building_path(@owner)
+    end
+
+    def show
+        @building = Building.find(params[:id])
+        @url = owner_building_path
+        if 
+    end
+
+    def create
+        @owner = Owner.find(params[:id])
+        @building = @Owner.buildings.build
+        @building.update_attributes(building_params)
+        if @building.save
+            redirect_to owner_building_path(@building.owner, @building)
+        else
+            flash[:message] = "Could not be saved."
+            redirect_to new_owner_building_path(@owner)
+        end
+    end
+
+    def update 
+        @url = owner_building_path
+        @building = Building.find(params[:id])
+        @building.update_attributes(building_params)
+        if @building.save
+            flash[:message] = "Building saved."
+            redirect_to owner_building_path(@building.owner, @building)
+        else
+            flash[:message] = "Building failed to save."
+            render :show
+        end
+    end
+
+    private
+
+    def building_params
+        params.require(:building).permit(:name, :address, :owner_id)
+    end
+
+end 
---
+++ app/controllers/owners_controller.rb
@@ -0,0 +1,31 @@
+class OwnersController < ApplicationController
+    def all
+        @owners = Owner.all
+    end
+
+    def new
+        @owner = Owner.new
+    end
+
+    def create
+        @owner = Owner.new(name: owner_params[:name], user: current_user)
+        if @owner.save
+            @owner.user.
+            redirect_to owner_path(@owner)
+        else
+            flash[:message] = "Owner failed to save."
+            render :new
+        end
+    end
+
+    def show
+        @owner = Owner.find(params[:id])
+        @buildings = @Owner.buildings.all
+    end
+
+    private
+
+    def owner_params
+        params.require(:owner).permit(:name)
+    end
+end
