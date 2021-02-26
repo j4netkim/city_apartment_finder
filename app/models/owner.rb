065685ef9a72d89db40067407e0fdf4d51ff596f
J K <jkim65@buffalo.edu>
2021-02-26 18:45:49 -0500

    update tables and fix drop table migrate file

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -3,4 +3,21 @@ class Owner < ApplicationRecord
     has_many :buildings
     has_many :apartments, through: :buildings
     validates :name, presence: true
+
+    has_secure_password
+    # adds password and password confirmation attribute 
+    # validates that a password is present 
+
+    def self.create_from_omniauth(auth)
+        User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.username = auth['info']['name']
+            u.email = auth['info']['email']
+            u.password = SecureRandom.hex(20)
+        end
+    end
+
+    def self.set_owner
+        self.role = :owner
+        self.save
+    end
 end
