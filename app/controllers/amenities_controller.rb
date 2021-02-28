15c57d86aa0a25dfd05c809a84e8e012d0fbad16
J K <jkim65@buffalo.edu>
2021-02-28 05:39:57 -0500

    add owners home

--- app/controllers/amenities_controller.rb
+++ app/controllers/amenities_controller.rb
@@ -2,11 +2,17 @@ class AmenitiesController < ApplicationController
     
     def index
         if params[:apartment_id] && @apartment = Apartment.find_by_id(params[:apartment_id])
-            @amenities = @apartment.amenities
+            @amenities = @apartment.amenities.ordered_by_rent
         else
-            @amenities = Amenity
+            @amenities = Amenity.ordered_by_rent
         end
     end
 
-    
+    def new
+        if params[:apartment_id] && @apartment = Apartment.find_by_id(params[:apartment_id])
+            @amenities = @apartment.amenities.build
+        else
+            @amenity = @Amenity.new
+            @amenities = Amenity.ordered_by_rent
+    end
 end
