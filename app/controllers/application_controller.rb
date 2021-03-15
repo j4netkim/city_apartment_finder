044a78310253c28a4173a222e0c800adab6c888b
J K <jkim65@buffalo.edu>
2021-03-15 17:30:35 -0400

    fix designs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 
 class ApplicationController < ActionController::Base
-    
+    include SessionsHelper
     helper_method :current_owner
 
     def log_in(owner)
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
     end
 
     def current_owner
-        @current_owner ||= Owner.find_by(id: session[:owner_id]) if session[:owner_id]
+        @current_owner ||= Owner.find_by(id: session[:owner_id]) 
     end
+
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,3 +1,5 @@
 module SessionsHelper
-
+    def logged_in?
+        !current_owner.nil?
+    end
 end
