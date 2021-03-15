9817822b5ea882dba29565486400ac6050e86e28
J K <jkim65@buffalo.edu>
2021-03-15 15:39:29 -0400

    edit flash message in sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
     
     helper_method :current_owner
 
+    def log_in(owner)
+        session[:owner_id] = owner.id
+    end
 
     def current_owner
         @current_owner ||= Owner.find_by(id: session[:owner_id]) if session[:owner_id]
